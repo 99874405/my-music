ac8679f933df48ba560896d77add3783a4b9f8fd
guofengwu <guofengwu1994@163.com>
2023-09-23 17:10:41 +0800

    优化脚手架

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,4 +1,3 @@
-// app/providers.tsx
 'use client'
 import { NextUIProvider } from '@nextui-org/react'
 export function Providers({ children }: { children: React.ReactNode }) {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,4 +19,5 @@ const config: Config = {
   darkMode: 'class',
   plugins: [nextui()],
 }
+
 export default config

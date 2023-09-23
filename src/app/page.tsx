028925367e578f2d3a5f3d6ca99448074cd29fdb
guofengwu.xhy <guofengwu1994@163.com>
2023-09-23 21:29:40 +0800

    优化脚手架

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Avatar } from '@nextui-org/react'
-export default () => {
+export default function App() {
   return (
     <>
       <div className="flex gap-4 items-center">

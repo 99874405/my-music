90f86a32ac029442c7ac7dfbcbc25e67007c7ffa
guofengwu.xhy <guofengwu1994@163.com>
2023-09-24 21:29:34 +0800

    优化

---
+++ src/providers/NextUIProvider.tsx
@@ -0,0 +1,5 @@
+'use client'
+import { NextUIProvider as Provider } from '@nextui-org/react'
+export function NextUIProvider({ children }: { children: React.ReactNode }) {
+  return <Provider children={children} />
+}

cc70ebe8d68faec17ef7e09f3cdd4708c4f638ad
guofengwu <guofengwu1994@163.com>
2023-09-28 15:30:54 +0800

    add usePlayer

--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -3,8 +3,8 @@ import { useRef, useCallback } from 'react'
 export function usePlayer() {
   const audio = useRef(typeof Audio === 'function' && new Audio()).current
   return {
-    play: useCallback(() => {
-      audio.src = '/mp3/李荣浩-获奖人.flac'
+    play: useCallback(audioSrc => {
+      audio.src = audioSrc ?? '/mp3/李荣浩-获奖人.flac'
       audio.play()
     }, []),
   }

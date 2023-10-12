490bd6a244817f4b5a125c646db00380038ca262
guofengwu <guofengwu1994@163.com>
2023-10-12 10:58:50 +0800

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
 'use client'
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Spinner } from '@nextui-org/react'
-import { usePlayer } from '../hooks/usePlayer'
-import { useReactive, useMount } from 'ahooks'
-import { renderSong } from '../utils/render'
-import { default as axios } from 'axios'
+import { usePlayer } from '../providers/PlayerControlProvider'
 
 export default function App() {
   const player = usePlayer()
-  const state = useReactive({
-    loading: true,
-    musicList: [],
-  })
-
-  useMount(() => {
-    axios('/mp3.json').then(resp => {
-      state.loading = false
-      state.musicList = resp.data
-    })
-  })
-
   return (
     <Table
       isStriped
@@ -26,15 +11,17 @@ export default function App() {
       aria-label="Example static collection table">
       <TableHeader>
         <TableColumn>歌曲</TableColumn>
+        <TableColumn>歌手</TableColumn>
         <TableColumn>专辑</TableColumn>
         <TableColumn>时长</TableColumn>
       </TableHeader>
       <TableBody
-        isLoading={state.loading}
+        isLoading={player.loading}
         loadingContent={<Spinner />}>
-        {state.musicList.map((item: { song: string; album: string; duration: string; }, index) => (
+        {player.data.map((item, index) => (
           <TableRow key={index}>
-            <TableCell>{renderSong(item.song, player)}</TableCell>
+            <TableCell>{item.title}</TableCell>
+            <TableCell>{item.artist}</TableCell>
             <TableCell>{item.album}</TableCell>
             <TableCell>{item.duration}</TableCell>
           </TableRow>

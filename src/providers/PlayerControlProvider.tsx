545d99801f80d5d8caf3f10a1c264e53b498c791
guofengwu <guofengwu1994@163.com>
2023-10-13 10:38:06 +0800

    5.9

--- src/providers/PlayerControlProvider.tsx
+++ src/providers/PlayerControlProvider.tsx
@@ -1,20 +1,25 @@
 'use client'
+import { useSetState, useMount, useUpdateEffect } from 'ahooks'
 import { createContext, useContext } from 'react'
-import { useSetState, useMount } from 'ahooks'
 const context = createContext(void 0)
 const Provider = context.Provider
 
 export const usePlayer = () => useContext(context)
 export function PlayerControlProvider({ children }: { children: React.ReactNode }) {
   const [state, setState] = useSetState({
-    data: [],
     loading: !0,
+    version: '',
+    data: [],
   })
+
+  useUpdateEffect(() => {
+    console.log('appVersion', state.version)
+  }, [state.version])
 
   useMount(() => {
     setState({
-      data: require('./mock.json'),
       loading: !1,
+      ...require('./mock.json'),
     })
   })
 

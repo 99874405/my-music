c09892c3ee5314bfd80c66256e9fce7a2b5f7b8b
guofengwu.xhy <guofengwu1994@163.com>
2023-10-13 20:08:22 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Image, Button, Divider, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Spinner, User } from '@nextui-org/react'
+import { Card, CardBody, Image, Button, Divider, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Spinner, User } from '@nextui-org/react'
 import { FcDebian, FcMusic } from 'react-icons/fc'
 import { FaHeadphones } from 'react-icons/fa'
 import { usePlayer } from '../providers/PlayerControlProvider'
@@ -34,31 +34,35 @@ export default function App() {
   return (
     <div className="container mx-auto p-5">
       <div className="mt-5">
-        <div className="flex gap-x-5">
-          <Image
-            width={175}
-            height={175}
-            alt=""
-            src="https://qpic.y.qq.com/music_cover/iaVNVUXOg73ua71Z0dtuz7Yk5H0BzPibuKcx6noUYeFme1JJ3jibxe5vQ/600?n=1" />
-          <div className="pt-2">
-            <div className="text-2xl">My Music: 新一代的听歌应用</div>
-            <div className="flex items-center mt-1 text-sm text-stone-500"><FcDebian />&nbsp;畅听音乐，发现新声音。</div>
-            <div className="flex items-center mt-1 text-sm text-stone-500"><FcDebian />&nbsp;Summer</div>
-            <div className="flex items-center my-2 text-sm text-stone-700"><Divider /></div>
-            <div className="flex items-center mt-2 text-sm text-stone-700"><FcMusic />&nbsp;不遗憾 - 李荣浩</div>
-            <div className="flex items-center mt-2 text-sm text-stone-700">
-              <Button
-                size="sm"
-                startContent={<FaHeadphones />}
-                className="bg-green-600 text-white"
-                onClick={() => player.play()}>
-                {player.isPlaying ? '暂停' : '播放'}音乐
-              </Button>
+        <Card>
+          <CardBody>
+            <div className="flex gap-x-5">
+              <Image
+                width={175}
+                height={175}
+                alt=""
+                src="https://qpic.y.qq.com/music_cover/iaVNVUXOg73ua71Z0dtuz7Yk5H0BzPibuKcx6noUYeFme1JJ3jibxe5vQ/600?n=1" />
+              <div className="pt-2">
+                <div className="text-2xl">My Music: 新一代的听歌应用</div>
+                <div className="flex items-center mt-1 text-sm text-stone-500"><FcDebian />&nbsp;不断更新的音乐内容，与朋友分享歌单。</div>
+                <div className="flex items-center mt-1 text-sm text-stone-500"><FcDebian />&nbsp;夏天漫步世界</div>
+                <div className="flex items-center my-2 text-sm text-stone-700"><Divider /></div>
+                <div className="flex items-center mt-2 text-sm text-stone-700"><FcMusic />&nbsp;不遗憾 - 李荣浩</div>
+                <div className="flex items-center mt-2 text-sm text-stone-700">
+                  <Button
+                    size="sm"
+                    startContent={<FaHeadphones />}
+                    className="bg-green-600 text-white"
+                    onClick={() => player.play()}>
+                    {player.isPlaying ? '暂停' : '播放'}音乐
+                  </Button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          </CardBody>
+        </Card>
       </div>
-      <div className="mt-10">
+      <div className="mt-5">
         <Table
           isStriped
           classNames={{ table: 'min-h-[400px]' }}

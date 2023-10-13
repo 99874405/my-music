f06e89c177ee407267bfaa9471971359b00e89f8
guofengwu <guofengwu1994@163.com>
2023-10-13 14:50:45 +0800

    wip

--- src/providers/PlayerControlProvider.tsx
+++ src/providers/PlayerControlProvider.tsx
@@ -21,7 +21,7 @@ export function PlayerControlProvider({ children }: { children: React.ReactNode 
   })
 
   const play = useMemoizedFn((musicIndex) => {
-    if (audioControl.src && musicIndex == void 0 /* 通过[播放音乐]按钮 触发音乐的播放与暂停*/) {
+    if (audioControl.src && musicIndex == void 0 /* 点击[播放音乐]按钮 控制音乐的播放与暂停 */) {
       audioControl.paused ? audioControl.play() : audioControl.pause()
       setState({ isPlaying: !audioControl.paused })
       return

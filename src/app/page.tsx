6b10b5dc38038f3f7db62e0f4b7c7360ddff3f36
guofengwu <guofengwu1994@163.com>
2023-10-13 09:02:12 +0800

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
-import { FiGithub, FiClock, FiHeadphones } from 'react-icons/fi'
-import { Image, Button } from '@nextui-org/react'
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Spinner, User } from '@nextui-org/react'
+import { Image, Button, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Spinner, User } from '@nextui-org/react'
+import { ImHappy, ImClock, ImHeadphones } from 'react-icons/im'
 import { usePlayer } from '../providers/PlayerControlProvider'
 import { useMemoizedFn } from 'ahooks'
 
@@ -36,17 +35,17 @@ export default function App() {
     <div className="p-5">
       <div className="flex gap-x-5">
         <Image
-          width={150}
-          height={150}
+          width={165}
+          height={165}
           src="https://qpic.y.qq.com/music_cover/iaVNVUXOg73ua71Z0dtuz7Yk5H0BzPibuKcx6noUYeFme1JJ3jibxe5vQ/600?n=1" />
-        <div className="flex flex-col justify-center">
+        <div className="pt-2">
           <div className="text-2xl">My Music: 新一代听歌方式</div>
-          <div className="flex items-center mt-3 text-sm text-gray-500"><FiGithub /> &nbsp;大家喊我小虎牙</div>
-          <div className="flex items-center mt-1 text-sm text-gray-500"><FiClock /> &nbsp;10/12 14:14</div>
-          <div className="flex items-center mt-3">
+          <div className="flex items-center mt-3 text-sm text-stone-500"><ImHappy /> &nbsp;大家喊我小虎牙</div>
+          <div className="flex items-center mt-1 text-sm text-stone-500"><ImClock /> &nbsp;10/12 14:14</div>
+          <div className="flex items-center mt-3 text-sm text-stone-500">
             <Button
               size="sm"
-              startContent={<FiHeadphones />}
+              startContent={<ImHeadphones />}
               className="bg-green-600 text-white">
               播放歌曲
             </Button>

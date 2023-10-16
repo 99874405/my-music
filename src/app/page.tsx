62db7c955db8f64e1032807413693ac81a0429fb
guofengwu <guofengwu1994@163.com>
2023-10-16 10:47:41 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function App() {
                 src="./image/cover.webp" />
               <div className="pt-2">
                 <div className="hidden sm:block sm:text-2xl">My Music: 新一代的听歌应用</div>
-                <div className="flex items-center mt-1 text-sm text-stone-500"><FcDebian />&nbsp;不断更新音乐内容</div>
+                <div className="flex items-center mt-1 text-sm text-stone-500"><FcDebian />&nbsp;夏天漫步世界</div>
                 <div className="flex items-center mt-1 text-sm text-stone-500"><FcDebian />&nbsp;Summer</div>
                 <div className="flex items-center my-2 text-sm text-stone-700"><Divider /></div>
                 <div className="flex items-center mt-2 text-sm text-stone-700"><FcMusic />&nbsp;{renderSong()}</div>

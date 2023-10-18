71e49e890a6e3464ef81c0ba7256c51114620c32
guofengwu.xhy <guofengwu1994@163.com>
2023-10-18 19:43:36 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Card, CardBody, Image, Button, Divider, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Spinner, User, Tooltip } from '@nextui-org/react'
+import { Navbar, NavbarBrand } from '@nextui-org/react'
 import { FaHeadphones, FaFastBackward, FaFastForward } from 'react-icons/fa'
 import { FcDebian, FcMusic } from 'react-icons/fc'
 import { usePlayer } from '../providers/PlayerControlProvider'
@@ -50,85 +51,91 @@ export default function App() {
   })
 
   return (
-    <div className="max-w-screen-lg mx-auto p-5">
-      <div className="mt-0">
-        <Card
-          style={{ backgroundImage: "url('./image/李荣浩.webp')" }}
-          className="bg-top bg-right bg-contain bg-no-repeat bg-[#eaeaea]">
-          <CardBody>
-            <div className="flex gap-x-5">
-              <Image
-                width={166}
-                height={166}
-                alt=""
-                src={player.currPlayMusic?.coverArt || './image/coverArt.webp'} />
-              <div className="pt-1">
-                <div className="hidden sm:block text-2xl">Next Music: 新一代听歌驿站</div>
-                <div className="flex items-center mt-1 text-sm text-stone-500"><FcDebian />&nbsp;夏天漫步世界</div>
-                <div className="flex items-center mt-1 text-sm text-stone-500"><FcDebian />&nbsp;Summer</div>
-                <div className="flex items-center my-2 text-sm text-stone-800"><Divider /></div>
-                <div className="flex items-center mt-2 text-sm text-stone-800"><FcMusic />&nbsp;{renderSong()}</div>
-                <div className="flex items-center mt-2 text-sm text-stone-800">
-                  <Button
-                    size="sm"
-                    startContent={<FaHeadphones />}
-                    className={`${!player.isPlaying ? 'bg-green-600' : 'bg-danger'} text-white`}
-                    disabled={player.dataLoading}
-                    onClick={() => player.play()}>
-                    {!player.isPlaying ? '播放' : '暂停'}
-                  </Button>
-                  {player.currPlayMusic && (
+    <>
+      <Navbar>
+        <NavbarBrand>
+          <p className="font-bold">Next</p>
+        </NavbarBrand>
+      </Navbar>
+      <div className="max-w-screen-lg mx-auto p-5 pt-0">
+        <div className="mt-0">
+          <Card
+            style={{ backgroundImage: `url('./image/${player.currPlayMusic?.artist || '李荣浩'}.webp')` }}
+            className="bg-top bg-right bg-contain bg-no-repeat bg-[#eaeaea]">
+            <CardBody>
+              <div className="flex gap-x-5">
+                <Image
+                  width={166}
+                  height={166}
+                  alt=""
+                  src={player.currPlayMusic?.coverArt || './image/coverArt.webp'} />
+                <div className="pt-2">
+                  <div className="flex items-center mt-1 text-sm text-stone-500"><FcDebian />&nbsp;夏天漫步世界</div>
+                  <div className="flex items-center mt-1 text-sm text-stone-500"><FcDebian />&nbsp;Summer</div>
+                  <div className="flex items-center my-5 text-sm text-stone-800"><Divider /></div>
+                  <div className="flex items-center mt-1 text-sm text-stone-800"><FcMusic />&nbsp;{renderSong()}</div>
+                  <div className="flex items-center mt-2 text-sm text-stone-800">
                     <Button
                       size="sm"
-                      isIconOnly
-                      className="ml-2"
+                      startContent={<FaHeadphones />}
+                      className={`${!player.isPlaying ? 'bg-green-600' : 'bg-danger'} text-white`}
                       disabled={player.dataLoading}
-                      onClick={() => player.playPrev()}>
-                      <FaFastBackward />
+                      onClick={() => player.play()}>
+                      {!player.isPlaying ? '播放' : '暂停'}
                     </Button>
-                  )}
-                  {player.currPlayMusic && (
-                    <Button
-                      size="sm"
-                      isIconOnly
-                      className="ml-2"
-                      disabled={player.dataLoading}
-                      onClick={() => player.playNext()}>
-                      <FaFastForward />
-                    </Button>
-                  )}
+                    {player.currPlayMusic && (
+                      <Button
+                        size="sm"
+                        isIconOnly
+                        className="ml-2"
+                        disabled={player.dataLoading}
+                        onClick={() => player.playPrev()}>
+                        <FaFastBackward />
+                      </Button>
+                    )}
+                    {player.currPlayMusic && (
+                      <Button
+                        size="sm"
+                        isIconOnly
+                        className="ml-2"
+                        disabled={player.dataLoading}
+                        onClick={() => player.playNext()}>
+                        <FaFastForward />
+                      </Button>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          </CardBody>
-        </Card>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="mt-5">
+          <Table
+            isStriped
+            classNames={{ table: 'min-h-[400px]' }}
+            aria-label="Example table with custom cells">
+            <TableHeader>
+              <TableColumn>歌曲</TableColumn>
+              <TableColumn>歌手</TableColumn>
+              <TableColumn>时长</TableColumn>
+            </TableHeader>
+            <TableBody
+              isLoading={player.dataLoading}
+              loadingContent={<Spinner />}>
+              {player.data.map((record, key) => (
+                <TableRow
+                  key={key}
+                  className="cursor-pointer"
+                  onClick={() => player.play(key)}>
+                  <TableCell>{renderCell(record, 'title')}</TableCell>
+                  <TableCell>{renderCell(record, 'artist')}</TableCell>
+                  <TableCell>{renderCell(record, 'duration')}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
       </div>
-      <div className="mt-5">
-        <Table
-          isStriped
-          classNames={{ table: 'min-h-[400px]' }}
-          aria-label="Example table with custom cells">
-          <TableHeader>
-            <TableColumn>歌曲</TableColumn>
-            <TableColumn>歌手</TableColumn>
-            <TableColumn>时长</TableColumn>
-          </TableHeader>
-          <TableBody
-            isLoading={player.dataLoading}
-            loadingContent={<Spinner />}>
-            {player.data.map((record, key) => (
-              <TableRow
-                key={key}
-                className="cursor-pointer"
-                onClick={() => player.play(key)}>
-                <TableCell>{renderCell(record, 'title')}</TableCell>
-                <TableCell>{renderCell(record, 'artist')}</TableCell>
-                <TableCell>{renderCell(record, 'duration')}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
-    </div>
+    </>
   )
 }

a678c55e87965a8f7cfb89790fe32192cb1eaf9b
guofengwu.xhy <guofengwu1994@163.com>
2023-10-19 18:57:05 +0800

    Update index.tsx

--- src/components/PlayCircleIcon/index.tsx
+++ src/components/PlayCircleIcon/index.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 export const PlayCircleIcon = ({ size = 24, width, height, ...props }) => (
   <svg
-  aria-hidden="true"
-  fill="none"
-  focusable="false"
-  height={size || height}
-  role="presentation"
-  viewBox="0 0 24 24"
-  width={size || width}
-  {...props}
+    viewBox="0 0 1024 1024"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    p-id="6633"
+    width={size}
+    height={size}
   >
-      <path
-        d="M8,0C3.582,0,0,3.582,0,8s3.582,8,8,8s8-3.582,8-8S12.418,0,8,0z M5,12V4l7,4L5,12z"
-        fill="currentColor"
-      />
+    <path
+      d="M512 80C273.536 80 80 273.536 80 512s193.536 432 432 432 432-193.536 432-432S750.464 80 512 80z m183.168 456.624l-273.456 157.68c-19.008 10.816-42.768-2.592-42.768-24.624V353.888c0-22.032 23.76-35.424 42.768-24.624l273.456 157.68c19.024 11.232 19.024 38.88 0 49.68z"
+      p-id="6634"
+      fill="#000000"
+    ></path>
   </svg>
 );
